42ab729d20a2f2c15f71ab072da179375e7fdaff
Zidan Abraham <zidangit07@gmail.com>
2024-05-12 11:29:44 +0800

    feat: implement db connection and load env file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"log"
 
+	"github.com/danzBraham/eniqilo-store/config"
+	"github.com/danzBraham/eniqilo-store/internal/db"
 	"github.com/danzBraham/eniqilo-store/internal/http/api"
 )
 
 func main() {
-	server := api.NewAPIServer("localhost:8080", nil)
+	env := config.LoadEnv()
+
+	dbpool, err := db.ConnectDB(env)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer dbpool.Close()
+
+	server := api.NewAPIServer("localhost:8080", dbpool)
 	if err := server.Run(); err != nil {
 		log.Panic(err)
 	}
